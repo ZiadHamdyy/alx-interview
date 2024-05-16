1f320db614291f4271108f4bc5248f7b84600bc2
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-05-17 00:08:12 +0300

    again

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,47 +1,73 @@
 #!/usr/bin/python3
-""" N queens problem """
+"""nQueens to add the Q when it possible."""
+import sys
 
 
-def nQueen(n):
-    """ N queens problem """
-    invalid = set()
-    pos = set()
-    neg = set()
-    res = []
-    board = [['.'] * n for _ in range(n)]
+def is_valid_place(board, row, column, n):
+    """Check if it's possible to place a queen at the given position."""
+    for j in range(column):
+        if board[row][j] == 'Q':
+            return False
 
-    def backtrack(row):
-        if row == n:
-            res.append([[i, j] for i in range(n)
-                        for j in range(n) if board[i][j] == 'Q'])
-            return
-        for col in range(n):
-            if col in invalid or (row - col) in neg or (row + col) in pos:
-                continue
-            invalid.add(col)
-            neg.add(row - col)
-            pos.add(row + col)
-            board[row][col] = 'Q'
-            backtrack(row + 1)
-            invalid.remove(col)
-            neg.remove(row - col)
-            pos.remove(row + col)
-            board[row][col] = '.'
-    backtrack(0)
-    return res
+    i, j = row, column
+    while i >= 0 and j >= 0:
+        if board[i][j] == 'Q':
+            return False
+        i -= 1
+        j -= 1
+
+    i, j = row, column
+    while i < n and j >= 0:
+        if board[i][j] == 'Q':
+            return False
+        i += 1
+        j -= 1
+
+    return True
 
 
-if __name__ == "__main__":
-    """ the entry point """
-    import sys
+def nqueens(board, n, column=0, solutions=[]):
+    """Solve the n-Queens problem."""
+    if column == n:
+        solution = []
+        for i in range(n):
+            for j in range(n):
+                if board[i][j] == 'Q':
+                    solution.append([i, j])
+        solutions.append(solution)
+        return
+
+    for i in range(n):
+        if is_valid_place(board, i, column, n):
+            board[i][column] = 'Q'
+            nqueens(board, n, column + 1, solutions)
+            board[i][column] = '.'
+
+
+def main():
+    """Main function to run the script."""
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    if not sys.argv[1].isdigit():
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
         print("N must be a number")
         sys.exit(1)
-    if int(sys.argv[1]) < 4:
+
+    if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-    for solution in nQueen(int(sys.argv[1])):
+
+    board = [['.' for _ in range(n)] for _ in range(n)]
+    solutions = []
+    nqueens(board, n, 0, solutions)
+
+    solutions.sort()
+    for solution in solutions:
         print(solution)
+
+
+if __name__ == "__main__":
+    main()
