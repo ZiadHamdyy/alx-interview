9675bc864da16a4668fc8dafe52e559356069bf1
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-05-22 11:36:45 +0300

    task

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
 
+const API_URL = 'https://swapi-api.hbtn.io/api';
 
 if (process.argv.length > 2) {
-  request(`${'https://swapi-api.hbtn.io/api'}/films/${process.argv[2]}/`,
-    (err, _, body) => {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
     if (err) {
       console.log(err);
     }
